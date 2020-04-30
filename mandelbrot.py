351cd5f15f424d2b899507bcd8e88c45f3fc0bcf
doleksiyenko <51185857+doleksiyenko@users.noreply.github.com>
2020-04-30 13:56:24 -0400

    Mandelbrot class file and main file

---
+++ mandelbrot.py
@@ -0,0 +1,26 @@
+import numpy as np
+from matplotlib import pyplot as plt
+from numba import jit
+
+class Mandelbrot:
+    """
+    A class for the generation of mandelbrot sets.
+
+
+
+    """
+    def __init__(self):
+        pass
+
+    @jit
+    def generate_fractal(self, width, height, max_iterations):
+        """
+        The mandelbrot set is symmetrical, therefore, only the top portion of
+        the set must be generated.
+
+        :return:
+        """
+        pass
+
+    def get_image(self):
+        pass
