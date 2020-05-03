f1a71dc26711617aaf01864d8a74d921ddb9dccc
doleksiyenko <51185857+doleksiyenko@users.noreply.github.com>
2020-05-03 13:09:26 -0400

    changed settings

--- settings.py
+++ settings.py
@@ -1,5 +1,9 @@
 import numpy as np
 
-_resolution = np.array([640, 480], np.int16)
+_resolution = np.array([1500, 1000], np.int16)
 _zoom = np.array([-0.743643887037158704752191506114774,
                   0.131825904205311970493132056385139], np.longdouble)
+
+# left, right, top, bottom
+bounds = ([-2, 1]
+          , [1, -1])
