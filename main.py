c817eb3cef85bc4d858c6db00a8a6bc851f16f5e
doleksiyenko <51185857+doleksiyenko@users.noreply.github.com>
2020-05-03 13:07:24 -0400

    added rendering time measurement, and image file naming

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 from mandelbrot import Mandelbrot
 from settings import _resolution, _zoom
+import time
 
 if __name__ == '__main__':
     _mandelbrot = Mandelbrot(resolution=_resolution, zoom=_zoom)
-    _mandelbrot.get_image()
+    print("Rendering...")
+    time_a = time.time()
+
+    _mandelbrot.get_image('set_1500_aspect')
+
+    time_b = time.time()
+    t = time_b - time_a
+    print("Rendering took: {} seconds / {} minutes.".format(t, (t / 60)))
