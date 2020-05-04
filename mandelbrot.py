7ef0c1c539f2390cdbfafc93c5e02e0f40c87ab5
doleksiyenko <51185857+doleksiyenko@users.noreply.github.com>
2020-05-04 12:11:50 -0400

    Removed class structure to so that JIT compilation works. Improves speed massively.

--- mandelbrot.py
+++ mandelbrot.py
@@ -1,73 +1,64 @@
-from settings import bounds
 from numba import jit
 from PIL import Image
 import numpy as np
+import time
 
 
-class Mandelbrot:
+@jit(nopython=True, fastmath=True)
+def generate_fractal(max_iterations, bounds) -> np.array:
     """
-    A class for the generation of mandelbrot sets.
+    The mandelbrot set is symmetrical, therefore, only the top portion of
+    the set must be generated.
 
+    Colour range from 0-255 gray-scale image.
+    Apply a sigmoid transformation, meaning smaller no. of iterations makes
+    pixel whiter
     """
-    def __init__(self, resolution: np.array):
-        # the resolution of the image
-        self.height = resolution[1]
-        self.width = resolution[0]
+    image_array = np.zeros((1000, 1500))
+    colour = 0
 
-    @jit
-    def generate_fractal(self, max_iterations) -> np.array:
-        """
-        The mandelbrot set is symmetrical, therefore, only the top portion of
-        the set must be generated.
+    # for every pixel in the image
+    for py in range(1000):
+        # print("Completed: {}/{} rows".format(py, 2000 - 1))
+        for px in range(1500):
+            # c is the coordinate related with the center of the pixel
+            # (px, py)
+            c = complex(bounds[0][0] + (0.5 + px) * (
+                        (bounds[0][1] - bounds[0][0]) / 1500),
+                        (bounds[1][1] - (0.5 + py) * (
+                            bounds[1][1] - bounds[1][0]) / 1000
+                         ))
+            z = complex(0, 0)
 
-        Colour range from 0-255 gray-scale image.
-        Apply a sigmoid transformation, meaning smaller no. of iterations makes
-        pixel whiter
-        """
-        image = np.zeros((self.height, self.width))
-        colour = 0
+            # test if the coordinate is bounded
+            for iteration in range(max_iterations):
+                z_n = z * z + c
+                z = z_n
 
-        # for every pixel in the image
-        for py in range(self.height):
-            # print("Completed: {}/{} rows".format(py, self.height - 1))
-            for px in range(self.width):
-                # c is the coordinate related with the center of the pixel
-                # (px, py)
-                c = complex(bounds[0][0] + (0.5 + px) * (
-                            (bounds[0][1] - bounds[0][0]) / self.width),
-                            (bounds[1][1] - (0.5 + py) * (
-                                bounds[1][1] - bounds[1][0]) / self.height
-                             ))
-                z = complex(0, 0)
+                if z.real**2 + z.imag**2 > 4:
+                    # then this pixel has escaped the bound, and colour the
+                    # pixel depending on how many iterations it took to
+                    # escape
 
-                # test if the coordinate is bounded
-                for iteration in range(max_iterations):
-                    z_n = z * z + c
-                    z = z_n
+                    colour = 255 - (iteration * np.log2(
+                        np.log2(z.real**2 + z.imag**2)))
+                    break
 
-                    if z.real**2 + z.imag**2 > 4:
-                        # then this pixel has escaped the bound, and colour the
-                        # pixel depending on how many iterations it took to
-                        # escape
+                if iteration == max_iterations - 1:
+                    # then this pixel is bounded
+                    colour = 255
 
-                        colour = 255 - (iteration * np.log2(
-                            np.log2(z.real**2 + z.imag**2)))
-                        break
+            image_array[py, px] = colour
 
-                    if iteration == max_iterations - 1:
-                        # then this pixel is bounded
-                        colour = 255
+    return image_array
 
-                image[py, px] = colour
 
-        return image
-
-    def get_image(self, name: str) -> None:
-        """
-        Generate the image of the mandelbrot, with a scale and center
-        coordinates
-        """
-        image = Image.fromarray(self.generate_fractal(max_iterations=255))
-        image = image.convert('L')
-        image.save('{}.png'.format(name))
-
+def get_image(name: str, bound) -> None:
+    """
+    Generate the image of the mandelbrot, with a scale and center
+    coordinates
+    """
+    frame_ = Image.fromarray(generate_fractal(max_iterations=350,
+                                              bounds=bound))
+    frame_ = frame_.convert('L')
+    frame_.save('{}.png'.format(name))
